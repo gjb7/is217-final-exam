5f56acfa65ee62ab0f6941b356560a8c60bacc35
Grant J. Butler <butler.grant.j@gmail.com>
2014-05-14 00:58:36 -0400

    Reworking import script.

--- import.js
+++ import.js
@@ -1,18 +1,5 @@
-var loadModels = require('./app/model');
+var ComponentOption = require('./app/model/ComponentOption'),
+	ComponentOptionGroup = require('./app/model/ComponentOptionGroup'),
+	Laptop = require('./app/model/Laptop');
 
-loadModels().then(function(models) {
-	insertComponentOptions(models.ComponentOption);
-});
 
-function insertComponentOptions(ComponentOption) {
-	var components = [
-		{ name: '11-inch', price: 0},
-		{ name: '15-inch', price: 20000},
-		{ name: '128 GB', price: 0},
-		{ name: '256 GB', price: 20000},
-		{ name: '8 GB', price: 0},
-		{ name: '16 GB', price: 20000}
-	];
-	
-	return ComponentOption.create.apply(null, components);
-}
