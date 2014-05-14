570a4fd0bf663eea282c18509ac49bb260312d41
Grant J. Butler <butler.grant.j@gmail.com>
2014-05-13 23:54:54 -0400

    Fetch saved quotes from mongo.

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,7 +1,11 @@
-var _ = require('koa-route');
+var Quote = require('../model/Quote');
 
 module.exports = function(app) {
-	app.use(_.get('/', function *() {
-		yield this.render('home');
-	}));
+	app.get('/', function *() {
+		var quotes = yield Quote.find({}).exec();
+		
+		yield this.render('home', {
+			quotes: quotes
+		});
+	});
 };
