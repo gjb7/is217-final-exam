8afc14c57d91c5cf7512174c341c0023764ec2c5
Grant J. Butler <butler.grant.j@gmail.com>
2014-05-13 20:34:11 -0400

    require mongodb.

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 var path = require('path'),
 	fs = require('fs'),
-	mongo = require('mongo'),
+	mongodb = require('mongodb'),
 	koa = require('koa'),
 	_ = require('koa-route'),
 	serve = require('koa-static'),
 	body = require('koa-body'),
 	swig = require('koa-swig'),
 	app = koa();
-
-
 
 swig(app, {
 	root: path.join(__dirname, 'app', 'views')
