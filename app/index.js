19f282e943b85707b8d32b0638f2906d1bbb01af
Grant J. Butler <butler.grant.j@gmail.com>
2014-05-13 21:58:55 -0400

    call callback after loading.

--- app/index.js
+++ app/index.js
@@ -1,12 +1,16 @@
 var path = require('path'),
 	fs = require('fs');
 
-module.exports = function(app) {
+module.exports = function(app, cb) {
 	var controllerDir = path.join(__dirname, 'controller');
 	
 	fs.readdir(controllerDir, function(err, files) {
 		files.forEach(function(file) {
 			require(path.join(controllerDir, file))(app);
 		});
+		
+		if (cb) {
+			cb();
+		}
 	});
 };
