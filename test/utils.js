f44a2a4e6bf87012b41155dd3301bc4dec0c6598
Grant J. Butler <butler.grant.j@gmail.com>
2014-05-14 17:18:30 -0400

    Setup mock objects.

--- test/utils.js
+++ test/utils.js
@@ -4,6 +4,26 @@ var mocha = require('mocha'),
 
 describe('utils', function() {
 	describe('#calculateCost', function() {
+		var laptop = {
+			name: 'Laptop',
+			baseCost: 1000
+		};
+		
+		var componentOptions = [
+			{
+				name: '16GB',
+				price: 200
+			},
+			{
+				name: '128GB',
+				price: 0
+			},
+			{
+				name: '15-inch',
+				price: 200
+			}
+		];
+		
 		it('should calculate the cost of a laptop with components', function() {
 			
 		});
