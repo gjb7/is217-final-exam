b9fc673d8f434fbb2f74a7ae5288d263f6ff0c8d
Grant J. Butler <butler.grant.j@gmail.com>
2014-05-14 10:38:39 -0400

    Transform title into slug before saving.

--- app/model/ComponentOptionGroup.js
+++ app/model/ComponentOptionGroup.js
@@ -4,10 +4,15 @@ var mongoose = require('mongoose'),
 
 var ComponentOptionGroupSchema = new Schema({
 	title: String,
+	slug: String,
 	componentOptions: [{
 		type: ObjectId,
 		ref: 'ComponentOption'
 	}]
+});
+
+ComponentOptionGroupSchema.pre('save', function(next) {
+	this.slug = title.toLowerCase().replace(/[ _]/g, '-').replace(/[^0-9a-z\-]/g, '');
 });
 
 module.exports = mongoose.model('ComponentOptionGroup', ComponentOptionGroupSchema);
