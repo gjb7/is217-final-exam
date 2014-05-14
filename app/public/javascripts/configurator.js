350866386aebcbb0fdeef45acf44b72852c3b406
Grant J. Butler <butler.grant.j@gmail.com>
2014-05-14 13:24:41 -0400

    Added code to finish dynamic generation.

--- app/public/javascripts/configurator.js
+++ app/public/javascripts/configurator.js
@@ -18,12 +18,42 @@
 	}
 	
 	Configurator.prototype._renderComponentOptionGroup = function(group) {
-		var fieldset = $('<fieldset />');
-		var legend = $('<legend />');
+		var fieldset = $('<fieldset />'),
+			legend = $('<legend />');
+		
 		legend.text(group.title);
 		fieldset.append(legend);
 		
+		var self = this;
+		
+		group.componentOptions.forEach(function(option) {
+			var optionElm = self._renderComponentOption(group, option);
+			
+			fieldset.append(optionElm);
+		})
+		
 		return fieldset;
+	}
+	
+	Configurator.prototype._renderComponentOption = function(group, option) {
+		var radioDiv = $('<div />');
+		radioDiv.addClass('radio');
+		
+		var label = $('<label />');
+		radioDiv.append(label);
+		
+		var input = $('<input />');
+		
+		input.attr({
+			type: 'radio',
+			name: group._id,
+			value: option._id
+		});
+		
+		label.append(input);
+		label.append(option.name);
+		
+		return radioDiv;
 	}
 	
 	global.Configurator = Configurator;
