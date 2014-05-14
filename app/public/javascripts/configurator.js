23d2b5e0b6f96deb5a1639e82edea5faedb1fb63
Grant J. Butler <butler.grant.j@gmail.com>
2014-05-14 13:19:55 -0400

    Moved configurator stuff into separat function.

--- app/public/javascripts/configurator.js
+++ app/public/javascripts/configurator.js
@@ -11,13 +11,19 @@
 		var self = this;
 		
 		this.data.componentOptionGroups.forEach(function(group) {
-			var fieldset = $('<fieldset />');
-			var legend = $('<legend />');
-			legend.text(group.title);
-			fieldset.append(legend);
+			var fieldset = self._renderComponentOptionGroup(group);
 			
 			self.configuratorElm.append(fieldset);
 		});
+	}
+	
+	Configurator.prototype._renderComponentOptionGroup = function(group) {
+		var fieldset = $('<fieldset />');
+		var legend = $('<legend />');
+		legend.text(group.title);
+		fieldset.append(legend);
+		
+		return fieldset;
 	}
 	
 	global.Configurator = Configurator;
