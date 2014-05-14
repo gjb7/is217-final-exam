c83b17a089b722811404d4c61c9e17ec643661af
Grant J. Butler <butler.grant.j@gmail.com>
2014-05-14 00:20:28 -0400

    Added routes for viewing, creating, and posting quotes.

--- app/controller/quote.js
+++ app/controller/quote.js
@@ -2,6 +2,14 @@ module.exports = function(app) {
 	app.get('/quote', function() {
 		
 		
+		yield this.render('view-quote');
+	});
+	
+	app.get('/quote/new', function() {
 		yield this.render('new-quote');
+	});
+	
+	app.post('/quote', function() {
+		
 	});
 };
