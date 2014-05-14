04306bc1eff7f00670af01c38cb4dd0ca3507239
Grant J. Butler <butler.grant.j@gmail.com>
2014-05-14 11:35:03 -0400

    populate laptop.

--- app/controller/home.js
+++ app/controller/home.js
@@ -2,7 +2,7 @@ var Quote = require('../model/Quote');
 
 module.exports = function(app) {
 	app.get('/', function *() {
-		var quotes = yield Quote.find({}).exec();
+		var quotes = yield Quote.find({}).populate('laptop').exec();
 		
 		yield this.render('home', {
 			quotes: quotes
