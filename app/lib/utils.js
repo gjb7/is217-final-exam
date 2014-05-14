8fdb811017129328812e7647b7d62dd99c16fdba
Grant J. Butler <butler.grant.j@gmail.com>
2014-05-14 17:08:33 -0400

    Added calculateCost function.

---
+++ app/lib/utils.js
@@ -0,0 +1,9 @@
+function calculateCost(laptop, componentOptions) {
+	var totalPrice = laptop.basePrice;
+		
+	componentOptions.forEach(function(option) {
+		totalPrice += option.price;
+	});
+	
+	return totalPrice;
+};
