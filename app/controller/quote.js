d788a10c7ddc504c3b552dab1cf0264ff979dcf1
Grant J. Butler <butler.grant.j@gmail.com>
2014-05-14 10:26:28 -0400

    Implemented some base stuff for creating quotes.

--- app/controller/quote.js
+++ app/controller/quote.js
@@ -1,15 +1,26 @@
+var Quote = require('../model/Quote'),
+	Laptop = require('../model/Laptop');
+
 module.exports = function(app) {
-	app.get('/quote', function *() {
-		
-		
-		yield this.render('view-quote');
-	});
-	
 	app.get('/quote/new', function *() {
-		yield this.render('new-quote');
+		var laptop = yield Laptop.findOne().populate('componentOptionGroups componentOptionGroups.componentOptions').exec();
+		
+		yield this.render('new-quote', {
+			laptop: laptop
+		});
 	});
 	
 	app.post('/quote/new', function *() {
 		
+		
+/* 		this.redirect('/quote/'); */
+	});
+	
+	app.get('/quote/([0-9a-z]+)', function *(id) {
+		var quote = yield Quote.findById(id).exec();
+		
+		yield this.render('view-quote', {
+			quote: quote
+		});
 	});
 };
