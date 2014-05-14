c516647feabb3b47d40f031c06703a7fd9ccbe26
Grant J. Butler <butler.grant.j@gmail.com>
2014-05-13 22:01:19 -0400

    removed references to mongodb.

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var path = require('path'),
 	fs = require('fs'),
-	MongoClient = require('mongodb').MongoClient,
 	koa = require('koa'),
 	serve = require('koa-static'),
 	body = require('koa-body'),
@@ -8,21 +7,15 @@ var path = require('path'),
 	logger = require('koa-logger'),
 	app = koa();
 
-MongoClient.connect('mongodb://localhost/is217-final-exam', function(err, db) {
-	if (err) {
-		throw err;
-	}
-	
-	swig(app, {
-		root: path.join(__dirname, 'app', 'view'),
-		ext: 'swig'
-	});
-	
-	app.use(logger());
-	app.use(serve(path.join(__dirname, 'app', 'public')));
-	app.use(body());
-	
-	require('./app')(app, function() {
-		app.listen(3000);
-	});
+swig(app, {
+	root: path.join(__dirname, 'app', 'view'),
+	ext: 'swig'
+});
+
+app.use(logger());
+app.use(serve(path.join(__dirname, 'app', 'public')));
+app.use(body());
+
+require('./app')(app, function() {
+	app.listen(3000);
 });
