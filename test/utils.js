3d5d133a00578a1ba9ea970bdebd1a6704ae68b6
Grant J. Butler <butler.grant.j@gmail.com>
2014-05-14 17:21:20 -0400

    move mock object declaration.

--- test/utils.js
+++ test/utils.js
@@ -5,24 +5,26 @@ var mocha = require('mocha'),
 
 describe('utils', function() {
 	describe('#calculateCost', function() {
-		var laptop = {
-			basePrice: 1000
-		};
-		
-		var componentOptions = [
-			{
-				price: 200
-			},
-			{
-				price: 0
-			},
-			{
-				price: 200
-			}
-		];
-		
 		it('should calculate the cost of a laptop with components', function() {
+			var laptop = {
+				basePrice: 1000
+			};
+			
+			var componentOptions = [
+				{
+					price: 200
+				},
+				{
+					price: 0
+				},
+				{
+					price: 200
+				}
+			];
+			
 			expect(utils.calculateCost(laptop, componentOptions)).to.equal(1400);
+			
+			
 		});
 	});
 });
