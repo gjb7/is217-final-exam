0a049a90ac5c0546e580db5afc71a11bd2c8e72c
Grant J. Butler <butler.grant.j@gmail.com>
2014-05-13 20:15:43 -0400

    Removed loading code from app.js

--- app.js
+++ app.js
@@ -14,12 +14,6 @@ swig(app, {
 app.use(serve(path.join(__dirname, 'public')));
 app.use(body());
 
-var appDir = path.join(__dirname, 'app');
-
-fs.readdir(appDir, function(err, files) {
-	files.forEach(function(file) {
-		require(path.join(appDir, file))(app);
-	});
-});
+require('./app')(app);
 
 app.listen(3000);
