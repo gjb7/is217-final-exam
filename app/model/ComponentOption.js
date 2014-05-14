fd1a51a7e93b6c2b12ac5efff1362c720f1ca7b2
Grant J. Butler <butler.grant.j@gmail.com>
2014-05-14 10:42:50 -0400

    Added slug to ComponentOption.

--- app/model/ComponentOption.js
+++ app/model/ComponentOption.js
@@ -3,7 +3,14 @@ var mongoose = require('mongoose'),
 
 var ComponentOptionSchema = new Schema({
 	name: String,
+	slug: String,
 	price: Number
+});
+
+ComponentOptionSchema.pre('save', function(next) {
+	this.slug = util.toSlug(this.name);
+	
+	next();
 });
 
 module.exports = mongoose.model('ComponentOption', ComponentOptionSchema);
