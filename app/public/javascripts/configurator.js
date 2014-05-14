596e84d1e47e6d88fb305721b88e167f8f571ad4
Grant J. Butler <butler.grant.j@gmail.com>
2014-05-14 13:18:09 -0400

    begin dynamic generation code.

--- app/public/javascripts/configurator.js
+++ app/public/javascripts/configurator.js
@@ -1,6 +1,23 @@
 (function(global, $) {
 	function Configurator(data) {
 		this.data = data;
+		
+		this.configuratorElm = $('#configurator');
+		
+		this._render();
+	}
+	
+	Configurator.prototype._render = function() {
+		var self = this;
+		
+		this.data.componentOptionGroups.forEach(function(group) {
+			var fieldset = $('<fieldset />');
+			var legend = $('<legend />');
+			legend.text(group.title);
+			fieldset.append(legend);
+			
+			self.configuratorElm.append(fieldset);
+		});
 	}
 	
 	global.Configurator = Configurator;
