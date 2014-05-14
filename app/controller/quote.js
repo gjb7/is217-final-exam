f1cdf29dcecccd5a690ed4d62545f57edd72fc2a
Grant J. Butler <butler.grant.j@gmail.com>
2014-05-14 00:23:33 -0400

    make sure we're using generator functions.

--- app/controller/quote.js
+++ app/controller/quote.js
@@ -1,15 +1,15 @@
 module.exports = function(app) {
-	app.get('/quote', function() {
+	app.get('/quote', function *() {
 		
 		
 		yield this.render('view-quote');
 	});
 	
-	app.get('/quote/new', function() {
+	app.get('/quote/new', function *() {
 		yield this.render('new-quote');
 	});
 	
-	app.post('/quote', function() {
+	app.post('/quote', function *() {
 		
 	});
 };
