fedfbeaa51054e0597a6551f7a183a9e240543f8
Grant J. Butler <butler.grant.j@gmail.com>
2014-05-14 17:26:10 -0400

    require ComponentOption.

--- test/model.js
+++ test/model.js
@@ -1,4 +1,5 @@
 var mocha = require('mocha'),
 	chai = require('chai'),
 	expect = chai.expect,
-	Laptop = require('../app/model/Laptop');
+	Laptop = require('../app/model/Laptop'),
+	ComponentOption = require('../app/model/ComponentOption');
