003a14523ff754a8a10644ded529a44fef38ead0
Grant J. Butler <butler.grant.j@gmail.com>
2014-05-13 22:09:42 -0400

    use promises.

--- app/index.js
+++ app/index.js
@@ -1,16 +1,24 @@
 var path = require('path'),
-	fs = require('fs');
+	fs = require('fs'),
+	promise = require('promise');
+
+var readdir = Promise.denodeify(fs.readdir);
 
 module.exports = function(app, cb) {
-	var controllerDir = path.join(__dirname, 'controller');
-	
-	fs.readdir(controllerDir, function(err, files) {
-		files.forEach(function(file) {
-			require(path.join(controllerDir, file))(app);
-		});
+	return new Promise(function(fulfill, reject) {
+		var controllerDir = path.join(__dirname, 'controller');
 		
-		if (cb) {
-			cb();
-		}
+		fs.readdir(controllerDir, function(err, files) {
+			if (err) {
+				reject(err);
+			}
+			else {
+				files.forEach(function(file) {
+					require(path.join(controllerDir, file))(app);
+				});
+				
+				fulfill();
+			}
+		});
 	});
 };
